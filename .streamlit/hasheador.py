ae5a5763c06161b90282e6966715badf1a634773
antonio <4ht0h10@gmail.com>
2025-08-12 17:18:16 +0200

    script para hashear los password de los usuarios permitidos

--- .streamlit/hasheador.py
+++ .streamlit/hasheador.py
@@ -1,12 +1,11 @@
 import streamlit_authenticator as stauth
 
 # Lista de contraseñas en texto plano
-PASSWORD = 'xxxxx'
+PASSWORD = 'xxxxxxx'
 
 # Hashearlas correctamente
 hashed_passwords = stauth.Hasher().hash(PASSWORD)
 # Resultado 1
 
 # Resultado 1
-print(f" - la contraseña haseada es: {hashed_passwords}")
-
+print(f" - la contraseña {PASSWORD} haseada es: {hashed_passwords}")
